3bf9a46e2b813d776192bf217f5786c425aa30e6
panattaponsum <panattapon.sum@gmail.com>
2026-06-18 09:02:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2852,9 +2852,9 @@ function buildReportDocumentHtml(siteData, bodyHtml) {
     <div class="word-header-footer" style="mso-element:footer" id="f1">
         <table class="signature">
             <tr>
-                <td class="sig-box"><div class="sig-line">ลงชื่อ ........................................</div><div class="sig-name">(........................................)</div><div>ผู้จัดทำรายงาน</div></td>
-                <td class="sig-box"><div class="sig-line">ลงชื่อ ........................................</div><div class="sig-name">(........................................)</div><div>ผู้ตรวจสอบ</div></td>
-                <td class="sig-box"><div class="sig-line">ลงชื่อ ........................................</div><div class="sig-name">(........................................)</div><div>ผู้อนุมัติ</div></td>
+                <td class="sig-box"><div class="sig-line">ลงชื่อ ........................................</div><div class="sig-name"><b>${currentUserFullName || ''}</b><</div><div>ผู้จัดทำรายงาน</div></td>
+                <td class="sig-box"><div class="sig-line">ลงชื่อ ........................................</div><div class="sig-name">(..............................................)</div><div>ผู้ตรวจสอบ</div></td>
+                <td class="sig-box"><div class="sig-line">ลงชื่อ ........................................</div><div class="sig-name">(..............................................)</div><div>ผู้อนุมัติ</div></td>
             </tr>
         </table>
     </div>
